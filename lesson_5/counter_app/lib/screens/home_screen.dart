69677f2a7202e03f0d596dd8b4634ddfa119fe25
3bort <78028202+3bort@users.noreply.github.com>
2022-07-18 15:30:16 +0200

    Lección 5 (parte 3)
    Añadir botón flotante con una acción

--- lesson_5/counter_app/lib/screens/home_screen.dart
+++ lesson_5/counter_app/lib/screens/home_screen.dart
@@ -7,18 +7,25 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('HomeScreen'),
-          elevation: 0,
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Clicks Counters', style: fontSize30),
+            Text('0', style: fontSize30),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Clicks Counters', style: fontSize30),
-              Text('0', style: fontSize30),
-            ],
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('Hola Mundo');
+        },
+      ),
+    );
   }
 }
