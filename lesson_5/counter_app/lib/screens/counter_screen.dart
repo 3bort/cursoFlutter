982e875d8eaea902f160c261f31ed5b7d40cde3b
3bort <78028202+3bort@users.noreply.github.com>
2022-07-19 09:24:34 +0200

    Lección 5 (parte 5)
    Añadido el counter_screen con StatefulWidget para actualizar el contenido de pantalla

---
+++ lesson_5/counter_app/lib/screens/counter_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    const fontSize30 = TextStyle(fontSize: 30);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterScreen'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Clicks Counters', style: fontSize30),
+            Text('$counter', style: fontSize30),
+          ],
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          counter++;
+          setState(
+              () {}); //Función anonima para hacer funcionar el statefulwidget, siempre pide dentro una función {}
+        },
+      ),
+    );
+  }
+}
