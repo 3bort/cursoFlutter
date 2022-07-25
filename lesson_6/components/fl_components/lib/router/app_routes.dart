0214d3fd65015b2b4bf1224c1b4830851b56229c
3bort <78028202+3bort@users.noreply.github.com>
2022-07-25 19:21:46 +0200

    Lección 6 (Componentes) - Modelo y Listado de rutas
    Aunque esto no es necesario, con ello, tenemos mayor flexibilidad en la aplicación a la hora de crear rutas con su icono... etc

--- lesson_6/components/fl_components/lib/router/app_routes.dart
+++ lesson_6/components/fl_components/lib/router/app_routes.dart
@@ -1,9 +1,42 @@
+import 'package:fl_components/models/models.dart';
 import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   //sin el guión delante es publica la clase
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+      route: 'home',
+      name: 'Home Screen',
+      screen: const HomeScreen(),
+      icon: Icons.home_max_sharp,
+    ),
+    MenuOption(
+      route: 'listview1',
+      name: 'List View 1 Screen',
+      screen: const Listview1Screen(),
+      icon: Icons.list,
+    ),
+    MenuOption(
+        route: 'listview2',
+        name: 'List View 2 Screen',
+        screen: const Listview2Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+      route: 'alert',
+      name: 'Alertas - Alert',
+      screen: const AlertScreen(),
+      icon: Icons.add_alert,
+    ),
+    MenuOption(
+      route: 'card',
+      name: 'Tarjetas - Card',
+      screen: const CardScreen(),
+      icon: Icons.credit_card,
+    ),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
