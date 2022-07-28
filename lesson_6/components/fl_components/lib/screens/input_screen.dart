c9933c160457ac5111e097e69d3771e6ae60b0ba
3bort <78028202+3bort@users.noreply.github.com>
2022-07-29 01:00:27 +0200

    Lección 6 (Componentes) - Estilos de los inputs
    Centralizar estilos de los inputs en nuestro fichero app_theme y pasar algunos valores haciendo un extract del widget.

--- lesson_6/components/fl_components/lib/screens/input_screen.dart
+++ lesson_6/components/fl_components/lib/screens/input_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputScreen extends StatelessWidget {
@@ -13,39 +14,12 @@ class InputScreen extends StatelessWidget {
           child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Column(
-                children: [
-                  TextFormField(
-                    autofocus: false,
-                    initialValue: '',
-                    textCapitalization: TextCapitalization.words,
-                    onChanged: (value) {
-                      print('value: $value');
-                    },
-                    validator: (value) {
-                      if (value == null) return 'Este campo es requerido';
-                      return value.length < 3 ? 'Mínimo de 3 caracteres' : null;
-                    },
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    decoration: const InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)
-                      ),
-                      hintText: 'Nombre del usuario',
-                      labelText: 'Nombre',
-                      helperText: 'Sólo letras',
-                      //prefixIcon: Icon (Icons.verified_user_outlined),
-                      suffixIcon: Icon (Icons.group_outlined),
-                      icon: Icon(Icons.assignment_ind_outlined),
-                      border: OutlineInputBorder(
-
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          topRight: Radius.circular(10))
-                      )
-                    ),
-                  )
+                children: const [
+                  CustomInputField(labelText: 'Nombre', hintText: 'Nombre del usuario',)
                 ],
               )),
         ));
   }
 }
+
+
--- lesson_6/components/fl_components/lib/theme/app_theme.dart
+++ lesson_6/components/fl_components/lib/theme/app_theme.dart
@@ -24,6 +24,20 @@ class AppTheme {
         elevation: 5,
       ),
 
+      inputDecorationTheme: const InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: primary),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: primary),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10)),
+
+        ),
+
+        border: OutlineInputBorder(
+           borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10))
+        )
+
+      ),
+
       //Elevating Button
 
       elevatedButtonTheme: ElevatedButtonThemeData(
