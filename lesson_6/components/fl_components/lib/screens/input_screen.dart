e141d8feea64cf51a3347f9791b8c54c65eb3c14
3bort <78028202+3bort@users.noreply.github.com>
2022-07-29 16:16:18 +0200

    Lección 6 (Componentes) - Widget Form
    Validación de Textfield con FormState y poder ocultar el teclado con FocusScope.

--- lesson_6/components/fl_components/lib/screens/input_screen.dart
+++ lesson_6/components/fl_components/lib/screens/input_screen.dart
@@ -6,6 +6,17 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Uso de keys para mantener la referencia, usado en este caso para el Form
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+    final Map<String, String> formValues = {
+      'first_name': 'Emilio',
+      'last_name': 'Bort',
+      'email': 'emilio@bort.com',
+      'password': '123456',
+      'role': 'Admin'
+    };
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Inputs y Forms'),
@@ -13,20 +24,42 @@ class InputScreen extends StatelessWidget {
         body: SingleChildScrollView(
           child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Column(
-                children: const [
-                  CustomInputField(labelText: 'Nombre', hintText: 'Nombre del usuario'),
-                  SizedBox(height:30),
-                  CustomInputField(labelText: 'Apellido', hintText: 'Apellido del usuario'),
-                  SizedBox(height:30),
-                  CustomInputField(labelText: 'Correo', hintText: 'Correo del usuario', keyboardType: TextInputType.emailAddress),
-                  SizedBox(height:30),
-                  CustomInputField(labelText: 'Contraseña', hintText: 'Contraseña del usuario', obscureText: true),
-                  SizedBox(height:30),
-                ],
+              child: Form(
+                key: myFormKey,
+                child: Column(
+                  children: [
+                    const CustomInputField(
+                        labelText: 'Nombre', hintText: 'Nombre del usuario'),
+                    const SizedBox(height: 30),
+                    const CustomInputField(
+                        labelText: 'Apellido',
+                        hintText: 'Apellido del usuario'),
+                    const SizedBox(height: 30),
+                    const CustomInputField(
+                        labelText: 'Correo',
+                        hintText: 'Correo del usuario',
+                        keyboardType: TextInputType.emailAddress),
+                    const SizedBox(height: 30),
+                    const CustomInputField(
+                        labelText: 'Contraseña',
+                        hintText: 'Contraseña del usuario',
+                        obscureText: true),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                        child: const SizedBox(
+                            width: double.infinity,
+                            child: Center(child: Text('Guardar'))),
+                        onPressed: () {
+                          FocusScope.of(context).requestFocus(FocusNode()); //Código para esconder el teclado quitando el focus
+                          if (!myFormKey.currentState!.validate()) {
+                            print('Formulario no válido');
+                            return;
+                          }
+                          print(formValues);
+                        })
+                  ],
+                ),
               )),
         ));
   }
 }
-
-
